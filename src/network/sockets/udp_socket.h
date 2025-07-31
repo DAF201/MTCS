f0120fa58d84ba6155cec016642fb49b2024115e
fangzhou ye <fangzhou.ye@fii-na.com>
2025-07-31 16:05:40 -0700

    rename variables

--- src/network/sockets/udp_socket.h
+++ src/network/sockets/udp_socket.h
@@ -8,7 +8,7 @@
 #include "socket_setup.h"
 
 using namespace std;
-class cpp_udp_socket_server
+class cpp_udp_socket
 {
 
 protected:
@@ -17,27 +17,27 @@ protected:
     {
         unique_ptr<char[]> data;
         int size = 0;
-        sockaddr_in client_addr;
+        sockaddr_in target_addr;
     };
 
     // socket info
-    SOCKET server_socket = INVALID_SOCKET;
+    SOCKET local_socket = INVALID_SOCKET;
     WORD sock_version;
     WSADATA WSA_data;
-    int server_port = 0;
+    int port = 0;
 
     atomic<bool> stop_flag;
 
 public:
-    cpp_udp_socket_server() : stop_flag(false)
+    cpp_udp_socket(int port = 0) : stop_flag(false)
     {
         sock_version = MAKEWORD(2, 2);
 
         socket_wsa_start();
 
-        server_socket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+        local_socket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
 
-        if (server_socket == INVALID_SOCKET)
+        if (local_socket == INVALID_SOCKET)
         {
             printf("Error creating socket\n");
             socket_wsa_end();
@@ -46,41 +46,41 @@ public:
 
         sockaddr_in server_addr = {};
         server_addr.sin_family = AF_INET;
-        server_addr.sin_port = htons(0);
+        server_addr.sin_port = htons(port);
         server_addr.sin_addr.s_addr = INADDR_ANY;
 
         // bind to an available port
-        if (bind(server_socket, (sockaddr *)&server_addr, sizeof(server_addr)) == SOCKET_ERROR)
+        if (bind(local_socket, (sockaddr *)&server_addr, sizeof(server_addr)) == SOCKET_ERROR)
         {
             printf("Bind failed: %d\n", WSAGetLastError());
-            closesocket(server_socket);
+            closesocket(local_socket);
             socket_wsa_end();
             throw runtime_error("Bind failed");
         }
 
         // get which port being assigned by os
         int server_addr_size = sizeof(server_addr);
-        if (getsockname(server_socket, (sockaddr *)&server_addr, &server_addr_size) == SOCKET_ERROR)
+        if (getsockname(local_socket, (sockaddr *)&server_addr, &server_addr_size) == SOCKET_ERROR)
         {
             printf("Failed to get socket info\n");
-            closesocket(server_socket);
+            closesocket(local_socket);
             socket_wsa_end();
             throw runtime_error("Fetch data failed");
         }
-        this->server_port = ntohs(server_addr.sin_port);
+        this->port = ntohs(server_addr.sin_port);
     }
 
     // this is a point to point UDP, so just let the child class handle the packet logic
-    bool send_packet(const char *data, int size, const sockaddr_in &client_address)
+    bool send_packet(const char *data, int size, const sockaddr_in &target_address)
     {
         if (stop_flag)
         {
             return false;
         }
 
-        int ret = sendto(server_socket, data, size, 0,
-                         reinterpret_cast<const sockaddr *>(&client_address),
-                         sizeof(client_address));
+        int ret = sendto(local_socket, data, size, 0,
+                         reinterpret_cast<const sockaddr *>(&target_address),
+                         sizeof(target_address));
         if (ret == SOCKET_ERROR)
         {
             printf("sendto failed: %d\n", WSAGetLastError());
@@ -101,11 +101,11 @@ public:
         {
             return socket_pkg();
         }
-        sockaddr_in client_address{};
-        int client_address_length = sizeof(client_address);
+        sockaddr_in target_address{};
+        int target_address_length = sizeof(target_address);
         char buffer[MAX_PACKET_LENGTH];
-        int ret = recvfrom(server_socket, buffer, MAX_PACKET_LENGTH, 0,
-                           (sockaddr *)&client_address, &client_address_length);
+        int ret = recvfrom(local_socket, buffer, MAX_PACKET_LENGTH, 0,
+                           (sockaddr *)&target_address, &target_address_length);
         if (ret == SOCKET_ERROR)
         {
             printf("recvfrom failed: %d\n", WSAGetLastError());
@@ -115,24 +115,29 @@ public:
         packet.size = ret;
         packet.data = make_unique<char[]>(ret);
         memcpy(packet.data.get(), buffer, ret);
-        packet.client_addr = client_address;
+        packet.target_addr = target_address;
         return packet;
+    }
+
+    int get_port()
+    {
+        return port;
     }
 
     void quit()
     {
-        string cmd = "powershell -Command \"$udpClient = New-Object System.Net.Sockets.UdpClient; $udpClient.Connect('127.0.0.1'," + to_string(server_port) + "); $bytes = [System.Text.Encoding]::ASCII.GetBytes('exit'); $udpClient.Send($bytes, $bytes.Length); $udpClient.Close()\"";
+        string cmd = "powershell -Command \"$udpClient = New-Object System.Net.Sockets.UdpClient; $udpClient.Connect('127.0.0.1'," + to_string(port) + "); $bytes = [System.Text.Encoding]::ASCII.GetBytes('exit'); $udpClient.Send($bytes, $bytes.Length); $udpClient.Close()\"";
         system(cmd.c_str());
     }
 
-    ~cpp_udp_socket_server()
+    ~cpp_udp_socket() noexcept
     {
         stop_flag = true;
-        if (server_socket != INVALID_SOCKET)
+        if (local_socket != INVALID_SOCKET)
         {
-            shutdown(server_socket, SD_BOTH);
-            closesocket(server_socket);
-            server_socket = INVALID_SOCKET;
+            shutdown(local_socket, SD_BOTH);
+            closesocket(local_socket);
+            local_socket = INVALID_SOCKET;
         }
         socket_wsa_end();
     }

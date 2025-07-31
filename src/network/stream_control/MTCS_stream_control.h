33938d716f53feba23b693ababe2a2cf750e3f9e
fangzhou ye <fangzhou.ye@fii-na.com>
2025-07-31 16:52:29 -0700

    UDP sample test pass

--- src/network/stream_control/MTCS_stream_control.h
+++ src/network/stream_control/MTCS_stream_control.h
@@ -1,6 +1,7 @@
 #include "../protocol/MTCS.h"
 #include "../sockets/tcp_server_socket_safe.h"
 #include "../sockets/tcp_client_socket_safe.h"
+#include "../sockets/udp_socket.h"
 #define MAX_PACKET_LENGTH 1056
 // this is the TCP controller socket, try to limite the size of data send, use the UDP sockets to transport data
 class MTCS_server_controller_socket : public cpp_tcp_socket_server
@@ -26,6 +27,14 @@ class MTCS_client_controller_socket : public cpp_tcp_socket_client
 {
 public:
     MTCS_client_controller_socket(string server_ip, int server_port) : cpp_tcp_socket_client(server_ip, server_port)
+    {
+    }
+};
+
+class MTCS_transportation_socket : public cpp_udp_socket
+{
+public:
+    MTCS_transportation_socket(int port = 0) : cpp_udp_socket(port)
     {
     }
 };

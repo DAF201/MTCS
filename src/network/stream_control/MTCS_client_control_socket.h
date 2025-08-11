fea1aac3e5279c9e16ce9c3a83ed85247fd67692
daf201 <daf201@blink-in.com>
2025-08-11 13:36:56 -0700

    Delete src directory

--- src/network/stream_control/MTCS_client_control_socket.h
+++
@@ -1,7 +0,0 @@
-#ifndef MTCS_CLIENT_CONTROL_SOCKET_HPP
-#define MTCS_CLIENT_CONTROL_SOCKET_HPP
-#include "../sockets/tcp_client_socket_safe.h"
-class MTCS_client_control_socket : cpp_tcp_socket_client
-{
-};
-#endif
--- src/network/stream_control/MTCS_server_control_socket.h
+++
@@ -1,7 +0,0 @@
-#ifndef MTCS_SERVER_CONTROL_SOCKET_HPP
-#define MTCS_SERVER_CONTROL_SOCKET_HPP
-#include "../sockets/tcp_server_socket_safe.h"
-class MTCS_server_control_socket : cpp_tcp_socket_server
-{
-};
-#endif
